502ec95ca07aab45fe01f444a49ce26f50bcf4cc
Vladimir Topolev <i.vladimir.topolev@gmail.com>
2021-05-13 11:33:57 +0300

    Fix build

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use('/static', express.static(path.resolve(__dirname, '../../static/static')));
+app.use('/static', express.static(path.resolve(__dirname, '../static/static')));
 
 app.get('*', (req, res) => {
-    const pathFile = path.resolve(__dirname, '../../static/index.html');
+    const pathFile = path.resolve(__dirname, '../static/index.html');
     res.sendFile(pathFile)
 });
 
